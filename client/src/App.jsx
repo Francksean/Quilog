9795d3b4ff070e5c3592a3d1b6ebb529d298c585
franck sean <seandjissou@gmail.com>
2024-01-11 13:44:40 +0100

    nouvelle base

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import "./App.css"
 import { Routes, Route } from 'react-router-dom'
-import Acceuil from "./pages/acceuil/acceuil"
-import Articles from "./pages/Articles/articles"
-import Connexion from "./pages/connexion/connexion"
-
+import Landing from "./pages/landing/landing"
 
 
 function App() {
@@ -11,12 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element= {<Acceuil/>}/>
-        <Route path='/Acceuil' element= {<Acceuil/>}/>
-        <Route path='/Articles' element= {<Articles/>}/>
-        <Route path='/auth/login' element= {<Connexion/>}/>
-        {/* <Route path='/user/:id' element= {<Home/>}/> */}
-        {/* <Route path='/profile' element= {<Profile/>}/> */}
+        <Route path="/" element={<Landing/>}/>
       </Routes>
     </>
   )
