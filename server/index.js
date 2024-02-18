69c53ad15cf5f6d7ea7fc4bbcccbe8a4c70200db
franck sean <seandjissou@gmail.com>
2024-02-18 23:28:13 +0100

    little change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import { userAuthRouter } from './Routes/usersAuth.js'
 import { userInfosRouter } from './Routes/usersInfos.js'
 import { articlesRouter } from './Routes/articles.js'
 
-
 const app = express()
 
 app.use(express.json())
@@ -15,10 +14,7 @@ app.use("/auth", userAuthRouter)
 app.use("/infos", userInfosRouter)
 app.use("/content", articlesRouter)
 
-mongoose.connect(
-    "mongodb+srv://seandjissou:adminquilog@quilogcluster.ivatx2e.mongodb.net/Quilog?retryWrites=true&w=majority"
-)
-
+mongoose.connect("mongodb+srv://seandjissou:adminquilog@quilogcluster.ivatx2e.mongodb.net/Quilog?retryWrites=true&w=majority")
 
 app.listen(3000,()=>{
     console.log("\n\n\n\n\n\n\nServer started on port 3000")
